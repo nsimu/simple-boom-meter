c06d0b568d331a4637eb703b899aa911f8808ecb
Thomas Hirsch <thokari@posteo.de>
2020-09-06 22:33:30 +0200

    add RMS mode, clean up

--- simple-boom-meter.ino
+++ simple-boom-meter.ino
@@ -1,21 +1,23 @@
 #include "LedControl.h"
 
-int LEDS_DIN = 12;
-int LEDS_CLK = 11;
-int LEDS_CS = 10;
-int NUM_DISPLAYS = 1;
+#define LEDS_DIN 12
+#define LEDS_CLK 11
+#define LEDS_CS 10
+#define NUM_DISPLAYS 1
 LedControl LEDS = LedControl(LEDS_DIN, LEDS_CLK, LEDS_CS, NUM_DISPLAYS);
 
 void setup() {
   analogReference(EXTERNAL);
+
   LEDS.shutdown(0, false);
-  LEDS.setIntensity(0,4);
+  LEDS.setIntensity(0, 4);
   LEDS.clearDisplay(0);
+
   Serial.begin(9600);
 }
 
-int MAX_SCALE = 9;
-byte NUM_LEDS[9] = {
+# define MAX_SCALE 9
+byte NUM_LEDS[MAX_SCALE] = {
   B00000000,
   B10000000,
   B11000000,
@@ -26,22 +28,28 @@ byte NUM_LEDS[9] = {
   B11111110,
   B11111111,
 };
-int MAX_PEAK_TO_PEAK = 400;
+byte mode = 0; // 0 = PeakToPeak, 1 = RootMeanSquares
+unsigned int maxInputsByMode[] = { 400, 75 };
+int value = 0;
 
-void loop(){
-  int value = getPeakToPeak();
+void loop() {
+  if (mode == 0) {
+    value = getPeakToPeak();
+  } else if (mode == 1) {
+    value = getRootMeanSquares();
+  }
   Serial.println(value);
-  int displayValue = map(value, 0, MAX_PEAK_TO_PEAK, 0, MAX_SCALE);
+  int displayValue = map(value, 0, maxInputsByMode[mode], 0, MAX_SCALE);
   for(int row = 0; row < 8; row++) {
     LEDS.setRow(0, row, NUM_LEDS[displayValue]);  
   }
 }
 
-unsigned int SAMPLE_WINDOW = 20; // ms
-int MIKE_A0 = A0;
+#define SAMPLE_WINDOW 20 // ms
+#define MIKE_A0 A0
 
 int getPeakToPeak() {
-  unsigned long startMillis= millis();
+  unsigned long startMillis = millis();
   unsigned int signalMax = 0;
   unsigned int signalMin = 1023;
   while (millis() - startMillis < SAMPLE_WINDOW) {
@@ -53,4 +61,19 @@ int getPeakToPeak() {
     }
   }
   return signalMax - signalMin;
+}
+
+#define BASELINE 283
+
+int getRootMeanSquares() {
+  unsigned long startMillis = millis();
+  unsigned long sumOfSquares = 0;
+  unsigned int numSamples = 0;
+  while (millis() - startMillis < SAMPLE_WINDOW) {
+    int sample = analogRead(MIKE_A0) - BASELINE;
+    numSamples++;
+    sumOfSquares += sq(sample);
+  }
+  int value = round(sqrt(sumOfSquares / numSamples));
+  return value;
 }
